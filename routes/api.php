69a7adc5595f5e0aba6a24a441b82738143b4d39
sandovaljohana <sandovaljohana0207@gmail.com>
2024-03-16 15:54:35 +0100

    create project with laravel

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Api\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/users', 'index');
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
